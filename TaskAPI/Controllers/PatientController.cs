425780f569c90467df63695bd3db08854f47ea1c
Андрей Николаев <Bender987@bk.ru>
2024-09-03 17:59:47 +0300

    add patient controller

--- TaskAPI/Controllers/PatientController.cs
+++ TaskAPI/Controllers/PatientController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using TaskAPI.Abstractions;
+using TaskAPI.Models.Pagination;
+using TaskAPI.Models;
 
 namespace TaskAPI.Controllers
 {
@@ -6,22 +10,111 @@ namespace TaskAPI.Controllers
     [Route("[controller]")]
     public class PatientController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private readonly IPatientRepository _patientRepository;
 
-        private readonly ILogger<PatientController> _logger;
-
-        public PatientController(ILogger<PatientController> logger)
+        public PatientController(IPatientRepository patientRepository)
         {
-            _logger = logger;
+            _patientRepository = patientRepository;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<int> Get()
+        [HttpGet]
+        public async Task<IActionResult> GetAsync(int id)
         {
-            return Enumerable.Range(1, 5);
+            var patient = await _patientRepository.GetForUpdateAsync(id);
+
+            if (patient != null)
+            {
+                return StatusCode(StatusCodes.Status200OK, patient);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
+        }
+
+        [HttpGet("List")]
+        public async Task<IActionResult> GetListAsync(PatientSort sort, [FromQuery] PaginationParams @params)
+        {
+            var patients = await _patientRepository.GetListAsync(sort);
+
+            var paginationMetadata = new PaginationMetadata(patients.Count, @params.Page, @params.ItemsPerPage);
+
+            Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(paginationMetadata));
+
+            var paginatedPatients = patients.Skip((@params.Page - 1) * @params.ItemsPerPage).Take(@params.ItemsPerPage);
+
+            if (paginatedPatients == null || paginatedPatients.Count() <= 0)
+            {
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status200OK, paginatedPatients);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync(
+            [FromQuery] string name,
+            [FromQuery] string surname,
+            [FromQuery] string patronymic,
+            [FromQuery] string address,
+            [FromQuery] DateOnly birthDate,
+            [FromQuery] string gender,
+            [FromQuery] int section
+            )
+        {
+            var patient = new Patient
+            {
+                Name = name,
+                Surname = surname,
+                Patronymic = patronymic,
+                Address = address,
+                BirthDate = birthDate,
+                Gender = gender,
+                Section = new Section() { Number = section }
+            };
+
+            var dbPatient = await _patientRepository.AddAsync(patient);
+
+            if (dbPatient != null)
+            {
+                return StatusCode(StatusCodes.Status200OK, dbPatient);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(int id, Patient patient)
+        {
+            var dbPatient = await _patientRepository.UpdateAsync(id, patient);
+
+            if (dbPatient != null)
+            {
+                return StatusCode(StatusCodes.Status200OK, dbPatient);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var patient = await _patientRepository.GetAsync(id);
+
+            (bool status, string message) = await _patientRepository.DeleteAsync(patient);
+
+            if (status == false)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, message);
+            }
+
+            return StatusCode(StatusCodes.Status200OK, patient);
         }
     }
 }
